f1cf65bf2bc101abe0191da1b020b8b7b48d928e
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-06-27 22:45:32 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Square from "./Square";
 
 export default function App() {
+  const [state, setState] = useState(Array(9).fill(null));
+
   return (
     <>
-      <div className="board_container"></div>
+      <div className="board_container">
+        <div className="board_row">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="board_row">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="board_row">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/Square.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Square = (props) => {
+  return (
+    <div
+      style={{
+        border: "1px solid ",
+        height: "100px",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        display: "flex",
+      }}
+      className="square">
+      <h5>{props.value}</h5>
+    </div>
+  );
+};
+export default Square;
